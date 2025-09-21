dab9a5f952c7afa58ec3423cfd085210d4a1362d
Max Novik <novik.kor@gmail.com>
2025-09-21 17:27:47 +0300

    made port and host configurable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+import os
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -36,5 +38,10 @@ app.include_router(users_router, prefix="/api")
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.getLogger().handlers = [RichHandler()]
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    load_dotenv()
+    uvicorn.run(
+        "main:app",
+        host=os.getenv("HOST", "127.0.0.1"),
+        port=int(os.getenv("PORT", 8000)),
+        reload=bool(os.getenv("DEVMODE") != "False"),
+    )
--- src/conf/config.py
+++ src/conf/config.py
@@ -33,7 +33,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()  # type: ignore
